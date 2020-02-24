918431dd19d6895b8bd0ac1cd401907565d4bf31
Valentin KAO <valentin.kao@unabiz.com>
2020-02-24 15:14:42 +0800

    ask path

--- main.py
+++ main.py
@@ -2,26 +2,36 @@
 import cv2
 import os
 
-filename = "ipman.mp4";
-cam = cv2.VideoCapture("./" + filename)
+
+def resize_frame(f, s):
+    return cv2.resize(f, s, interpolation=cv2.INTER_LINEAR)
+
+
 frame_array = []
+filename = input("Enter the full path of the file : ");
+try:
+    cam = cv2.VideoCapture(filename)
+    width = cam.get(cv2.CAP_PROP_FRAME_WIDTH)
+    height = cam.get(cv2.CAP_PROP_FRAME_HEIGHT)
+    size = (int(3840), int(3840 * height / width))
 
-while (True):
-    ret, frame = cam.read()
-    if ret:
-        height, width, layers = frame.shape
-        newHeight = int(3840 * height / width)
-        newWidth = int(3840)
-        size = (newWidth, newHeight)
-        frame = cv2.resize(frame, (newWidth, newHeight), interpolation=cv2.INTER_LINEAR)
-        frame_array.append(frame)
-    else:
-        break
+    while (True):
+        ret, frame = cam.read()
+        if ret:
+            frame_array.append(resize_frame(frame, size))
+        else:
+            break
 
-out = cv2.VideoWriter("4k" + filename, cv2.VideoWriter_fourcc(*'MP4V'), 30, size)
-for i in range(len(frame_array)):
-    out.write(frame_array[i])
+    out = cv2.VideoWriter("4k" + filename, cv2.VideoWriter_fourcc(*'MP4V'), 30, size)
+    for i in range(len(frame_array)):
+        out.write(frame_array[i])
+    out.release()
+    cam.release()
+    cv2.destroyAllWindows()
 
-out.release()
-cam.release()
-cv2.destroyAllWindows()
+except cv2.error as e:
+    print("Error: Wrong file path")
+
+
+except:
+    print("Error")
