d2e3057c6fc0ae6e97e71afb3be4ffe6b52b8bac
Valentin KAO <valentin.kao@unabiz.com>
2020-02-24 08:55:44 +0800

    frame to video fix

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import cv2
 import os
 
 filename = "ipman.mp4";
-# Read the video from specified path
 cam = cv2.VideoCapture("./" + filename)
 frame_array = []
 
@@ -11,17 +10,15 @@ while (True):
     ret, frame = cam.read()
     if ret:
         height, width, layers = frame.shape
-        print(width)
-        newHeight = int((3840 * width) / 2160)
+        newHeight = int(3840 * height / width)
         newWidth = int(3840)
         size = (newWidth, newHeight)
-        print(size)
         frame = cv2.resize(frame, (newWidth, newHeight), interpolation=cv2.INTER_LINEAR)
         frame_array.append(frame)
     else:
         break
 
-out = cv2.VideoWriter("4k" + filename, cv2.VideoWriter_fourcc(*'MP4V'), 60, size)
+out = cv2.VideoWriter("4k" + filename, cv2.VideoWriter_fourcc(*'MP4V'), 30, size)
 for i in range(len(frame_array)):
     out.write(frame_array[i])
 
