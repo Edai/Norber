37fdb74987fb3503541546a2bc2f61d47e7fbed7
Valentin KAO <valentin.kao@unabiz.com>
2020-02-24 08:48:30 +0800

    frame to video

--- main.py
+++ main.py
@@ -2,35 +2,29 @@
 import cv2
 import os
 
+filename = "ipman.mp4";
 # Read the video from specified path
-cam = cv2.VideoCapture("./ipman.mp4")
-
-try:
-    # creating a folder named data
-    if not os.path.exists('data'):
-        os.makedirs('data')
-    # if not created then raise error
-except OSError:
-    print ('Error: Creating directory of data')
-
-# frame
-currentframe = 0
+cam = cv2.VideoCapture("./" + filename)
+frame_array = []
 
 while (True):
-    # reading from frame
     ret, frame = cam.read()
     if ret:
-        # if video is still left continue creating images
-        name = './data/frame' + str(currentframe) + '.jpg'
-        print ('Creating...' + name)
-        # writing the extracted images
-        cv2.imwrite(name, frame)
-        # increasing counter so that it will
-        # show how many frames are created
-        currentframe += 1
+        height, width, layers = frame.shape
+        print(width)
+        newHeight = int((3840 * width) / 2160)
+        newWidth = int(3840)
+        size = (newWidth, newHeight)
+        print(size)
+        frame = cv2.resize(frame, (newWidth, newHeight), interpolation=cv2.INTER_LINEAR)
+        frame_array.append(frame)
     else:
         break
 
-# Release all space and windows once done
+out = cv2.VideoWriter("4k" + filename, cv2.VideoWriter_fourcc(*'MP4V'), 60, size)
+for i in range(len(frame_array)):
+    out.write(frame_array[i])
+
+out.release()
 cam.release()
 cv2.destroyAllWindows()
